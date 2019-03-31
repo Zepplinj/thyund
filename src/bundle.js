1bb6c820f4544bda26da2e34760eb3f146557a2c
Quitcher <GDquitcher@gmail.com>
2019-04-01 12:22:34 +1300

    Lmao Box

--- src/bundle.js
+++ src/bundle.js
@@ -1,2 +1,8 @@
+// Materialize
+import './css/materialize.scss'
+import './js/materialize'
+
+import './js/jquery-3.3.1'
+
 import './js/index'
 import './css/master.scss'
--- src/js/index.coffee
+++ src/js/index.coffee
@@ -1,1 +1,2 @@
 console.log 'Hello World'
+M.AutoInit
